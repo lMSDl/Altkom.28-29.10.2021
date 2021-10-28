4052b93be889d02ab1ec1349ea9d17cbd449a86c
Paweł Łęczycki <pawel@msd.info.pl>
2021-10-28 10:45:31 +0200

    Budowa programu

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// USING - deklarujemy, że będziemu yżuwać klas spod wskazanego adresu (przestrzeni nazw)
+using System;
+
+// PRZESTRZEŃ NAZW - jest to swego rodzaju adres dla klas
+namespace ConsoleApp
+{
+    // KLASA - opisuje zachowania i właściwości obiektów, które będą z niej wytwarzane
+    class Program
+    {
+        // METODA - typ zwracany + nazwa + nawiasy + opcjonalne parametry + ciało
+        // STATIC - oznacza, że jest to metoda niezależna (niezmienna) od instancji klasy
+        static void Main(string[] args)
+        {
+            
+        }
+    }
+}
