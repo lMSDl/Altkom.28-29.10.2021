08b3a51d27bf78bb0993631f0fff204326b49e2d
Paweł Łęczycki <pawel@msd.info.pl>
2021-10-29 10:09:48 +0200

    Tworzenie własnych klas

---
+++ Models/Product.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Models
+{
+    //public - ogólnie dostępne
+    //private - dostępne tylko w obrębie klasy
+    //internal - dostępne w obrębie projektu (assembly)
+    public class Product
+    {
+        private decimal _price;
+        /*private string _name;
+        public void SetName(string value)
+        {
+            _name = value;
+        }
+        public string GetName()
+        {
+            return _name;
+        }*/
+
+        //auto-property
+        public string Name { get; set; }
+
+        //full-property
+        public decimal Price
+        {
+            get { 
+                return _price;
+            }
+            set
+            {
+                if(value >= 0)
+                    _price = value;
+            }
+        }
+
+        public Product()
+        {
+
+        }
+        public Product(DateTime expirationDate)
+        {
+            ExpirationDate = expirationDate;
+        }
+
+        //read-only property (bez settera)- wartość możemy ustawić tylko w konstruktorze
+        public DateTime ExpirationDate { get; }
+
+        public override string ToString()
+        {
+            return $"{Name}; {Price}zł; {ExpirationDate.ToShortDateString()}";
+        }
+    }
+}
---
+++ ShopApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Models;
+
+namespace ShopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //inicjalizator (to w nawiasach klamrowych) - pozwala zainicjować publiczne właściwości
+            var prodcut1 = new Product(DateTime.Now.AddDays(5)) { Name = "masło", Price = 5.34M };
+
+            var prodcut2 = new Product();
+
+            Console.WriteLine("Podaj nazwę produktu:");
+            prodcut2.Name = Console.ReadLine();
+
+            do
+            {
+                Console.WriteLine("Podaj cenę produktu:");
+                if (decimal.TryParse(Console.ReadLine(), out var price))
+                {
+                    prodcut2.Price = price;
+                }
+                else
+                {
+                    Console.WriteLine("Błędna cena");
+                }
+            } while (prodcut2.Price == 0);
+
+            Console.WriteLine(prodcut1);
+            Console.WriteLine(prodcut2);
+        }
+    }
+}
